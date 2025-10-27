092e2d4f960404ba9f6848b817f3065bb6724a12
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-27 12:05:26 +0100

    add pin into the image and create macroareas for the logic of the exercise

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+// Milestone 3
+// Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+
+// Creo una variabile per richiamare il nodo della DOM riferito a "row"
+
+// Effettuo una chiamata AJAX con fetch per estrarre i dati dall'API
+
+// Dopo averli estratti ciclo dentro all'array per poter interagire con ogni singolo elemento
+
+// - Destrutturo l'oggetto e salvo dentro una variabile ogni proprietà
+
+// -Creo il nodo della DOM corrispondente alla polaroid
+// -Creo un array in cui salvo le classi del div principale "col-12 col-md-6 col-lg-4"
+// - Creo un div
+// - Al div aggiungo le classi salvate nella variabile usando l'operatore spread
+
+// - Creo un altro div
+//  a cui do classe "polaroid" 
+// e lo appendo dentro il div creato precedentemente
+
+// Creo l'immagine a cui do la classe "pin"
+// imposto l'src staticamente sull'url della foto
+
+// Creo l'immagine 
+// gli aggiungo la classe "user-img"
+// imposto l'src dinamicamente col template literal
+// Aggiungo il nodo al div "polaroid"
+
+// Creo il primo paragrafo (data)
+// Aggiungo il testo col template literal
+// Aggiungo il nodo al div "polaroid"
+
+// Creo il secondo paragrafo (testo)
+// Aggiungo il testo col template literal
+// Aggiungo il nodo al div "polaroid"
+
+// Inserisco dentro l'elemento row dinamicamente la colonna che abbiamo creato
